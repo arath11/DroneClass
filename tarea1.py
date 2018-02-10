567d49e2ae6dc46254c1aa13d843438067ad27b9
arath11 <35709739+arath11@users.noreply.github.com>
2018-02-09 22:00:38 -0600

    Add files via upload

--- tarea1.py
+++ tarea1.py
@@ -45,30 +45,30 @@ arm_and_takeoff(20)
 drone.airspeed = 10
 
 #Primer punto
-parada1 = LocationGlobalRelative(20.737395, -103.456960, 20)
+parada1 = LocationGlobalRelative(20.7374535, -103.4571305, 20)
 print("El despegue fue exitoso")
 print("EL dron va al primer vertice")
 #dice que ira al punto
 drone.simple_goto(parada1)
-time.sleep(25)
+time.sleep(31)
 print("Vertice 1 completado")
 print("Volando al vertice dos")
 #Segundo punto
-parada2 = LocationGlobalRelative(20.737370, -103.456715, 20)
+parada2 = LocationGlobalRelative(20.7374018, -103.4565112, 20)
 #dice que ira al punto
 drone.simple_goto(parada2)
 time.sleep(25)
 print("Vertice 2 completado")
 print("Volando al velrtice 3")
 #Tercer punto
-parada3 = LocationGlobalRelative(20.7370528, -103.4567228, 20)
+parada3 = LocationGlobalRelative(20.7369534, -103.4565377, 20)
 #Dice que ira al punto 
 drone.simple_goto(parada3)
 time.sleep(25)
 print("Vertice 3 completado")
 print("Volando al vertice 4")
 #Cuarto punto
-parada4 = LocationGlobalRelative(20.737084, -103.456945, 20)
+parada4 = LocationGlobalRelative(20.7369985, -103.4571415, 20)
 #Dice que ira al punto
 drone.simple_goto(parada4)
 time.sleep(25)
@@ -83,4 +83,5 @@ print("Regresando a punto de despegue")
 drone.mode = VehicleMode("RTL")
 print("El dron regreso exitosamente")
 #se muestra la bateria 
+print("Battery level:")
 print(drone.battery.voltage, "v")
